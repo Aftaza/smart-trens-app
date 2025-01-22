da8e1b666aa0405df4ba251468e30d0b42dd6014
Aftaza <taftazani17@gmail.com>
2025-01-22 17:21:06 +0700

    bypass rule

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { useAuth } from "@/firebase/AuthContext";
 import Link from "next/link";
 
 export default function Home() {
-  const {user, _loading} = useAuth()
-  
+  const {user, loading} = useAuth()
+  console.log(loading)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <div className="flex flex-col z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
